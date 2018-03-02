8a1c285ce5e9ebb8869991b7544dac3dba07bd34
Kevin Hartman <kevin@hart.mn>
2018-03-02 17:42:16 -0500

    Improve testability of CassandraExtensions.

--- project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraExtensions.scala
+++ project-fortis-spark/src/main/scala/com/microsoft/partnercatalyst/fortis/spark/sinks/cassandra/CassandraExtensions.scala
@@ -1,21 +1,19 @@
 package com.microsoft.partnercatalyst.fortis.spark.sinks.cassandra
 
-import com.datastax.spark.connector.cql.{CassandraConnector, Schema}
+import com.datastax.spark.connector.cql.{CassandraConnector, Schema, TableDef}
 import com.datastax.spark.connector.writer._
-import org.apache.spark.rdd.{PairRDDFunctions, RDD}
+import org.apache.spark.rdd.RDD
 
 import scala.reflect.ClassTag
 
 object CassandraExtensions {
   implicit class CassandraRDD[K, V](val rdd: RDD[(K, V)]) extends AnyVal {
-    def deDupValuesByCassandraTable(keyspaceName: String, tableName: String)
+    def deDupValuesByCassandraTable(keyspaceName: String, tableName: String, tableDefOpt: Option[TableDef] = None)
       (implicit connector: CassandraConnector = CassandraConnector(rdd.sparkContext), rwf: RowWriterFactory[V], kt: ClassTag[K], vt: ClassTag[V], ord: Ordering[K]): RDD[(K, V)] =
     {
-      val tableDef = Schema.tableFromCassandra(connector, keyspaceName, tableName)
+      val tableDef = tableDefOpt.getOrElse(Schema.tableFromCassandra(connector, keyspaceName, tableName))
       val rowWriter = implicitly[RowWriterFactory[V]].rowWriter(tableDef, tableDef.primaryKey.map(_.ref))
       val primaryKeySize = tableDef.primaryKey.length
-
-      //import org.apache.spark.rdd.PairRDDFunctions
 
       rdd.groupByKey().mapValues(eventRows => {
         eventRows.groupBy(value => {
